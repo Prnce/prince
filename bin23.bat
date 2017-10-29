bc1aa838bdc6873b6aa1d4eb1288827c304c4037
Prnce <amirhassan1975@yahoo.com>
2017-10-29 18:57:59 +0200

    Add files via upload

---
+++ bin23.bat
@@ -0,0 +1,6 @@
+@echo OFF
+taskkill /F /IM URLHelper.exe
+mode con:cols=30 lines=1
+title onlinesport
+COLOR 0A
+livestreamer --http-header "User-Agent=Sportstv204" "hlsvariant://http://185.180.15.249:8081/nimble_live/beinsports3-foot/playlist.m3u8?wmsAuthSign=c2VydmVyX3RpbWU9MTAvMjkvMjAxNyA0OjUzOjA4IFBNJmhhc2hfdmFsdWU9bTR1Q1NNd0NuVjlTVHRKMTM4VlBLZz09JnZhbGlkbWludXRlcz0yMA==" best
